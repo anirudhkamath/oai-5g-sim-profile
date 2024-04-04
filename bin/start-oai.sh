d2e739d9343284c169f039b7fa3aff253d65e550
Anirudh Kamath <anirudhkamath@hotmail.com>
2024-04-03 21:51:56 -0600

    change: only have setup scripts do the executable building

--- bin/start-oai.sh
+++ bin/start-oai.sh
@@ -1,7 +1,5 @@
 set -ex
 export DISPLAY=:1
-icwd=$(pwd)
-echo $icwd
 
 sudo killall nr-uesoftmodem nr-softmodem iperf3 oai-watchdog.sh || true
 
@@ -13,5 +11,5 @@ cd cmake_targets
 ./build_oai -c -C -I --ninja
 ./build_oai -c -C --gNB -w USRP --build-lib all --ninja
 
-sudo bash -c "touch ~/gnb_logs_$1"  # expect an experiment ID passed as an argument to the sh script.
-xterm -e bash -c "cd /opt/openairinterface5g/cmake_targets; sudo ./ran_build/build/nr-softmodem --sa -O /local/repository/etc/gnb.conf -d  > ~/gnb_logs_$1 2>&1 &" &
+sudo bash -c "touch ~/gnb_logs"  # expect an experiment ID passed as an argument to the sh script.
+# xterm -e bash -c "cd /opt/openairinterface5g/cmake_targets; sudo ./ran_build/build/nr-softmodem --sa -O /local/repository/etc/gnb.conf -d  > ~/gnb_logs 2>&1 &" &
